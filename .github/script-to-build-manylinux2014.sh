061ef0d65a8d245523a122bf1856c7709f8daecf
Cocoa <i@uwucocoa.moe>
2024-01-08 19:36:09 +0700

    added riscv64-linux-gnu

--- .github/script-to-build-manylinux2014.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -x
-
-OPENSSL_VERSION=$1
-ARCH=$2
-
-sudo docker run --privileged --network=host --rm -v `pwd`:/work "quay.io/pypa/manylinux2014_$ARCH:latest" \
-    sh -c "chmod a+x /work/do-build.sh && /work/do-build.sh ${OPENSSL_VERSION} ${ARCH} ${ARCH}-linux-gnu"
