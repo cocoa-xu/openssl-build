b48470912a3920fe2fe8a8ba806fd8942cbce12b
Cocoa <i@uwucocoa.moe>
2024-01-08 18:21:54 +0700

    fix build script

--- .github/script-to-build-manylinux2014.sh
+++ .github/script-to-build-manylinux2014.sh
@@ -6,4 +6,4 @@ OPENSSL_VERSION=$1
 ARCH=$2
 
 sudo docker run --privileged --network=host --rm -v `pwd`:/work "quay.io/pypa/manylinux2014_$ARCH:latest" \
-    sh -c "chmod a+x /work/do-build-manylinux2014.sh && /work/do-build-manylinux2014.sh ${OPENSSL_VERSION} ${ARCH} ${ARCH}-linux-gnu"
+    sh -c "chmod a+x /work/do-build.sh && /work/do-build.sh ${OPENSSL_VERSION} ${ARCH} ${ARCH}-linux-gnu"
