050d31fd5736a4bfa140d2489de090aef57648c0
Cocoa <i@uwucocoa.moe>
2024-01-08 16:27:52 +0700

    no tests

--- .github/do-build-manylinux2014.sh
+++ .github/do-build-manylinux2014.sh
@@ -12,7 +12,8 @@ mkdir -p "${PERFIX_DIR}" && \
     curl -fSL https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz -o openssl-${OPENSSL_VERSION}.tar.gz && \
     tar xf openssl-${OPENSSL_VERSION}.tar.gz && \
     cd openssl-${OPENSSL_VERSION} && \
-    ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" && make -j`nproc` && \
+    ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" no-tests && \
+    make -j`nproc` && \
     make -j`nproc` install_sw && \
     make -j`nproc` install_ssldirs && \
     cd "/openssl-${ARCH}-linux-gnu" && \
