d423535094c78f3368de08ee64f089982b989ce9
Cocoa <i@uwucocoa.moe>
2024-10-13 17:11:38 +0100

    updated ci
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/do-build.sh
+++ .github/do-build.sh
@@ -53,7 +53,7 @@ cd "openssl-${OPENSSL_VERSION}"
 case $OPENSSL_VERSION in
   3.* )
     case $TRIPLET in
-      *i386* )
+      *i386* | *i686* )
         ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" no-tests linux-generic32 -m32
         ;;
       * )
@@ -63,7 +63,7 @@ case $OPENSSL_VERSION in
     ;;
   1.1.1* )
     case $TRIPLET in
-      *i386* )
+      *i386* | *i686* )
         ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" linux-generic32 -m32
         ;;
       * )
