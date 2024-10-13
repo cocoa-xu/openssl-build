ba47496e9a3c5871a1651e81a3d2d298e24be344
Cocoa <i@uwucocoa.moe>
2024-10-13 15:33:18 +0100

    fix ci
    Signed-off-by: Cocoa <i@uwucocoa.moe>

--- .github/do-build.sh
+++ .github/do-build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -x
+set -eux
 
 OPENSSL_VERSION=$1
 ARCH=$2
@@ -8,73 +8,81 @@ TRIPLET=$3
 PERFIX_DIR="/openssl-${TRIPLET}"
 
 case $TRIPLET in
-     riscv64-linux-gnu )
-          apt-get update && \
-               apt-get install -y gcc g++ curl make automake autoconf libncurses5-dev perl python3
-          ;;
-     *-linux-gnu )
-          yum install -y curl automake autoconf openssl-devel ncurses-devel perl-IPC-Cmd python3
-          if [ "${TRIPLET}" = "s390x-linux-gnu" ]; then
-               yum install -y perl-App-cpanminus && \
-               cpanm Math::BigInt ;
-          fi
-          ;;
-     *-linux-musl )
-          apk add make curl gcc g++ perl linux-headers
-          ;;
-     * )
-          echo "Unknown triplet: ${TRIPLET}"
-          exit 1
-          ;;
+  riscv64-linux-gnu )
+    apt-get update && \
+    apt-get install -y gcc g++ curl make automake autoconf libncurses5-dev perl python3
+    ;;
+  armv7l-linux-gnueabihf )
+    apt-get update && \
+    apt-get install -y gcc g++ curl make automake autoconf libncurses5-dev perl python3
+    ;;
+  *-linux-gnu )
+    yum install -y curl automake autoconf openssl-devel ncurses-devel perl-IPC-Cmd python3
+    if [ "${TRIPLET}" = "s390x-linux-gnu" ]; then
+      yum install -y perl-App-cpanminus && \
+      cpanm Math::BigInt ;
+    fi
+    ;;
+  *-linux-musl )
+    apk add make curl gcc g++ perl linux-headers
+    ;;
+  * )
+    echo "Unknown triplet: ${TRIPLET}"
+    exit 1
+    ;;
 esac
 
 mkdir -p "${PERFIX_DIR}"
 cd /
 case $OPENSSL_VERSION in
-     3.* )
-          curl -fSL "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz" -o "openssl-${OPENSSL_VERSION}.tar.gz"
-          ;;
-     1.1.1* )
-          curl -fSL "https://openssl.org/source/old/1.1.1/openssl-${OPENSSL_VERSION}.tar.gz" -o "openssl-${OPENSSL_VERSION}.tar.gz"
-          ;;
-     * )
-          echo "Unknown version: ${OPENSSL_VERSION}"
-          exit 1
-          ;;
+  3.* )
+    curl -fSL "https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz" -o "openssl-${OPENSSL_VERSION}.tar.gz"
+    ;;
+  1.1.1* )
+    curl -fSL "https://openssl.org/source/old/1.1.1/openssl-${OPENSSL_VERSION}.tar.gz" -o "openssl-${OPENSSL_VERSION}.tar.gz"
+    ;;
+  * )
+    echo "Unknown version: ${OPENSSL_VERSION}"
+    exit 1
+    ;;
 esac
 
-tar xf openssl-${OPENSSL_VERSION}.tar.gz
-cd openssl-${OPENSSL_VERSION}
+tar -xf "openssl-${OPENSSL_VERSION}.tar.gz"
+cd "openssl-${OPENSSL_VERSION}"
 
 case $OPENSSL_VERSION in
-     3.* )
-          case $TRIPLET in
-               *i386* )
-                    ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" no-tests linux-generic32 -m32
-                    ;;
-               * )
-                    ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" no-tests
-                    ;;
-          esac
-          ;;
-     1.1.1* )
-          case $TRIPLET in
-               *i386* )
-                    ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" linux-generic32 -m32
-                    ;;
-               * )
-                    ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" linux-generic64
-                    ;;
-          esac
-          ;;
-     * )
-          echo "Unknown version: ${OPENSSL_VERSION}"
-          exit 1
-          ;;
+  3.* )
+    case $TRIPLET in
+      *i386* )
+        ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" no-tests linux-generic32 -m32
+        ;;
+      * )
+        ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" no-tests
+        ;;
+    esac
+    ;;
+  1.1.1* )
+    case $TRIPLET in
+      *i386* )
+        ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" linux-generic32 -m32
+        ;;
+      * )
+        ./Configure --prefix="${PERFIX_DIR}" --openssldir="${PERFIX_DIR}" linux-generic64
+        ;;
+    esac
+    ;;
+  * )
+    echo "Unknown version: ${OPENSSL_VERSION}"
+    exit 1
+    ;;
 esac
 
 make -j$(nproc) && \
-     make -j$(nproc) install_sw && \
-     make -j$(nproc) install_ssldirs && \
-     cd "/openssl-${TRIPLET}" && \
-     tar -czf "/work/openssl-${TRIPLET}.tar.gz" .
+  make -j$(nproc) install_sw && \
+  make -j$(nproc) install_ssldirs
+
+cd "/openssl-${TRIPLET}"
+tar -czf "/work/openssl-${TRIPLET}.tar.gz" .
+
+cd /work
+shasum -a 256 "openssl-${TRIPLET}.tar.gz" | tee "openssl-${TRIPLET}.tar.gz.sha256"
--- .github/script-to-build-gnu.sh
+++ .github/script-to-build-gnu.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
-set -x
+set -eux
 
 OPENSSL_VERSION=$1
 ARCH=$2
-IMAGE_NAME="quay.io/pypa/manylinux_2_28_$ARCH:latest"
+IMAGE_NAME=${3:-"quay.io/pypa/manylinux_2_28_$ARCH:latest"}
+DOCKER_PLATFORM=${4:-""}
 
-if [ "${ARCH}" = "riscv64" ]; then
-    IMAGE_NAME="riscv64/ubuntu:22.04"
+TARGET="${ARCH}-linux-gnu"
+if [ "${ARCH}" = "armv7l" ]; then
+  TARGET="armv7l-linux-gnueabihf"
 fi
 
-sudo docker run --privileged --network=host --rm -v $(pwd):/work "${IMAGE_NAME}" \
-    sh -c "chmod a+x /work/do-build.sh && /work/do-build.sh ${OPENSSL_VERSION} ${ARCH} ${ARCH}-linux-gnu"
+if [ ! -z "${DOCKER_PLATFORM}" ]; then
+  sudo docker run --privileged --network=host --rm --platform="${DOCKER_PLATFORM}" -v $(pwd):/work "${IMAGE_NAME}" \
+    sh -c "chmod a+x /work/do-build.sh && /work/do-build.sh ${OPENSSL_VERSION} ${ARCH} ${TARGET}"
+else
+  sudo docker run --privileged --network=host --rm -v $(pwd):/work "${IMAGE_NAME}" \
+    sh -c "chmod a+x /work/do-build.sh && /work/do-build.sh ${OPENSSL_VERSION} ${ARCH} ${TARGET}"
+fi
